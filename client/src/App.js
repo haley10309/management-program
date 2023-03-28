bae5fc84603c29067763ae52de7feaabd6008b4e
hayung <yoohayung10309@gmail.com>
2023-03-29 00:02:02 +0900

    for comparison

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ const styles = theme => ({
 
 
 class App extends Component {
-
+  // server에서 데이터를 받아오는 방법
   state = {//component 내에서 변경될 수 있는 변수
     customers: ""
   }
@@ -30,12 +30,15 @@ class App extends Component {
     this.callApi()
       .then(res => this.setState({customers : res}))
       .catch(err => console.log(err))
+
+    console.log('얍');
   }
 
   callApi = async () => { //비동기적으로 데이터 가져옴
     const response = await fetch('/api/customers'); //접속하고자 하는 api 주소
     const body = await response.json(); // 고객 목록을 json 형태로 body 변수에 넣음.
     return body;
+
   }
 
 
@@ -56,7 +59,7 @@ class App extends Component {
           </TableHead>
 
           <TableBody>
-          
+          {/* 서버에서 -> state에서 데이터 받으면 -> Customer에 저장 */}
           {this.state.customers? this.state.customers.map(c =>  { 
             return ( <Customer  key = {c.id } id = {c.id } image = {c.image} name ={c.name } birthday ={c.birthday}  gender ={c.gender} job ={c.job}/>);
             }): ""}
