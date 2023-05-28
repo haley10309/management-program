b5e6c06c8fbfa201ba683ff3aa2ba78a44b94658
hayung <yoohayung10309@gmail.com>
2023-05-28 21:33:11 +0900

    search .js commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 // 사용자 입장에서 볼수 있는 api에 출력
