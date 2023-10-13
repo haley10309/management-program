062d5d4c485bf3281c6f379cd9763f107e8a05e8
hayung <yoohayung10309@gmail.com>
2023-10-13 13:00:34 +0900

    CREATE customer database table&node.js Databses Connection settting

--- server.js
+++ server.js
@@ -2,41 +2,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = require('fs');
 
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
+const data = fs.readFileSync('client/database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user : conf.user,
+  password : conf.password,
+  port : conf.port,
+  database : conf.database
+});
+connection.connect();
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // 사용자 입장에서 볼수 있는 api에 출력
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/any",
-      name: "김민수",
-      birthday: "961222",
-      gender: "남자",
-      job: "개발자",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/any",
-      name: "박현규",
-      birthday: "930412",
-      gender: "남자",
-      job: "프로그래머",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/any",
-      name: "한연수",
-      birthday: "090822",
-      gender: "여자",
-      job: "디자이너",
-    },
-  ]);
+  connection.query(
+    "SELECT * FROM management.CUSTOMER",
+    (err,rows,fields)=> {
+      res.send(rows);
+    }
+  )
 });
 
 //동작 중인지 확인
